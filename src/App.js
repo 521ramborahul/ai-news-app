b766ff4471c0da6a812df65a82348350131a4714
kamlesh kumar <kamunvr522001@gmail.com>
2022-08-11 17:30:13 +0530

    news app

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import logo from './images/logo.jpg';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '304566301d0493fa291a8e4bafbd0a072e956eca572e1d8b807a3e2338fdd0dc/stage',
+      
+      
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newsHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://i.ytimg.com/vi/-GwqB4_berI/maxresdefault.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/kamlesh-kumar-930504196/"> kamlesh kumar</a>__ 
+            <a className={classes.link} href="https://github.com/521ramborahul"> 521ramborahul</a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="" />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
